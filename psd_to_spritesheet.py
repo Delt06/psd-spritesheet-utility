fb27016db8dbe6ba7ac250652d43c5f1195fa57f
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-06-02 16:17:26 +0300

    Fix wrong canvas size in PSD

--- psd_to_spritesheet.py
+++ psd_to_spritesheet.py
@@ -1,5 +1,6 @@
 import sys
 import subprocess
+from subprocess import Popen, PIPE
 
 psd_path = './input.psd'
 
@@ -11,5 +12,35 @@ for i, arg in enumerate(sys.argv):
         psd_path = arg
         continue
 
-subprocess.call('magick convert "%s" -delete 0 +append "%s_spritesheet.png"' % (psd_path, psd_path))
-subprocess.call('magick convert "%s" -delete 0 +append "%s_spritesheet.psd"' % (psd_path, psd_path))
+proc = Popen(
+    'magick identify -format "%w+"' +  ' "%s"' % (psd_path, ),
+    shell=True,
+    stdout=PIPE, stderr=PIPE
+)
+
+proc.wait()
+res = proc.communicate()  # получить tuple('stdout', 'stderr')
+if proc.returncode:
+    print(res[1])
+
+number_of_layers = str(res[0]).count('+')
+print('Detected %d effective layers.' % (number_of_layers - 1, ))
+
+def get_temp_file_name(index):
+    return '__temp_layer_' + str(index) + '.png'
+
+print('Creating temp layers...')
+
+for i in range(1, number_of_layers):
+    file_name = get_temp_file_name(i)
+    command = 'magick convert "%s[0]" "%s[%d]" "(" -clone 0 -alpha transparent ")" -swap 0 +delete -coalesce -compose src-over -composite %s' % (psd_path, psd_path, i, file_name)
+    subprocess.call(command)
+
+temp_file_names = [get_temp_file_name(i) for i in range(1, number_of_layers)]
+temp_images_joint = ' '.join(['"%s"' % (name, ) for name in temp_file_names])
+
+print('Creating PNG...')
+subprocess.call('magick convert %s +append "%s_spritesheet.png"' % (temp_images_joint, psd_path))
+print('Creating PSD...')
+subprocess.call('magick convert %s +append "%s_spritesheet.psd"' % (temp_images_joint, psd_path))
+print('Done.')
